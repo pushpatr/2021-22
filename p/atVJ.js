e76020038a335c536d9a1faa376c1382fd1904c0
pushpatr <87959439+pushpatr@users.noreply.github.com>
2021-08-12 10:10:47 +0530

    added 06 attn

--- p/atVJ.js
+++ p/atVJ.js
@@ -31,59 +31,33 @@ $(function(){
 		{
 			t:14,
 			c:"pbn-vfya-quq",
-			d:1
+			f:1
 		},{
 			t:14,
 			c:"pkw-ucwa-mqo",
-			d:2
+			f:1
 		},{
 			t:14,
 			c:"ing-kzir-xyt",
-			d:4,
+			f:1,
 		},{
 			t:14,
 			c:"wcz-xpua-eyg",
-			d:5,
+			f:1,
 		}
 	]
-/*
 
-	let msg = "";
-	let cd = "";
-	let tm = "";
-
-	function displayLateMSG() {
-		
-	}
-
+	
 	let findDay = classList.find(o => o.d == day);
-
+	
+	let classStartTimeTotal = findDay.t;
 
 	if(!findDay){
 		msg = "You don't have Hindi Class Today";
+		
 	}
 	else{
 		cd = findDay.c;
 		tm = findDay.t;
 	}
-	if( ( (hr == (tm - 1)) && (min>49) ) || ( (hr == tm) && (min<=40) ) ){
-		displayJoinMSG();
-	}
-	else if ( ( (hr == (tm - 1)) && (min < 49) ) || (hr < (tm - 1)) ){
-		displayEarlyMSG();
-	}
-	else if ( ( (hr == tm) && (min > 40) ) || (hr > tm) ){
-		displayLateMSG();
-	}
-
-	const scriptURL = 'https://script.google.com/macros/s/AKfycbxX_5ul6yVffkcXd6KafDy-v59HWaTZcOrBzpWOFNVn4tD11MMNHrrLHnlMmhz1Ix7epw/exec'
-	const form = document.forms['myenq'];
-
-	form.addEventListener('submit', e => {
-		e.preventDefault()
-		$("#msg").html("Please Wait...");
-		fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-			.then(response => alert("Data Submitted Successfully!!"))
-			.catch(error => console.error('Error!', error.message))
-	});*/
 });
